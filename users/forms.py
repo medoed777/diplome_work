85881d2a58111a0039ddf3d1f5440938b3f68283
Alexey <mister.skorpion4ik@mail.ru>
2025-10-26 18:45:34 +0500

    collected forms

---
+++ users/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+import re
+
+
+class CodeForm(forms.Form):
+    code = forms.CharField(max_length=4, label="Код")
+
+
+class PhoneLoginForm(forms.Form):
+    """Форма для входа по номеру телефона"""
+    phone = forms.CharField(
+        label="Номер телефона",
+        max_length=20,
+        widget=forms.TextInput(
+            attrs={
+                "type": "tel",
+                "class": "form-control form-control-lg",
+                "placeholder": "+7 (999) 999-99-99",
+                "id": "phone",
+            }
+        ),
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(PhoneLoginForm, self).__init__(*args, **kwargs)
+
+
+    def clean_phone(self):
+        phone = self.cleaned_data["phone"]
+        cleaned_phone = "".join(char for char in phone if char.isdigit() or char == "+")
+        if not cleaned_phone.startswith("+"):
+            raise forms.ValidationError("Номер телефона должен начинаться с '+'")
+        if len(cleaned_phone) > 20:
+            raise forms.ValidationError(
+                "Номер телефона не должен превышать 20 символов"
+            )
+        if not re.match(r"^\+7\d{10}$", cleaned_phone):
+            raise forms.ValidationError(
+                "Номер телефона должен быть в формате +7XXXXXXXXXX"
+            )
+        return cleaned_phone
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,12 @@
 from users.apps import UsersConfig
 # from users.views import RegisterView, VerifyCodeView, UserProfileView
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView
 
 
 app_name = UsersConfig.name
+
+
+urlpatterns = [
+    path("", include("users.urls", namespace="users")),
+]
